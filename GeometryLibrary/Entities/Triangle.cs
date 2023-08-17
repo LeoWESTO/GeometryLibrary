95eb3ad5021aa0667d3e6877b56487e590f9f0af
Amir Zagirov <zagirov_amir@mail.ru>
2023-08-17 17:41:13 +0300

    Добавьте файлы проекта.

---
+++ GeometryLibrary/Entities/Triangle.cs
@@ -0,0 +1,31 @@
+﻿using GeometryLibrary.Interfaces;
+
+namespace GeometryLibrary.Entities
+{
+    public class Triangle : IShape
+    {
+        private double side1;
+        private double side2;
+        private double side3;
+
+        public Triangle(double side1, double side2, double side3)
+        {
+            this.side1 = side1;
+            this.side2 = side2;
+            this.side3 = side3;
+        }
+
+        public double GetArea()
+        {
+            double semiPerimeter = (side1 + side2 + side3) / 2;
+            return Math.Sqrt(semiPerimeter * (semiPerimeter - side1) * (semiPerimeter - side2) * (semiPerimeter - side3));
+        }
+
+        public bool IsRightTriangle()
+        {
+            return Math.Pow(side1, 2) + Math.Pow(side2, 2) == Math.Pow(side3, 2) ||
+                   Math.Pow(side1, 2) + Math.Pow(side3, 2) == Math.Pow(side2, 2) ||
+                   Math.Pow(side2, 2) + Math.Pow(side3, 2) == Math.Pow(side1, 2);
+        }
+    }
+}
---
+++ GeometryLibraryTests/ShapeTests.cs
@@ -0,0 +1,42 @@
+﻿using GeometryLibrary.Entities;
+
+namespace GeometryLibraryTests
+{
+    [TestFixture]
+    public class ShapeTests
+    {
+        [Test]
+        public void CircleGetArea_ReturnsCorrectValue()
+        {
+            Circle circle = new Circle(5);
+            double expectedArea = 78.53981633974483;
+            double actualArea = circle.GetArea();
+            Assert.AreEqual(expectedArea, actualArea, 0.000001);
+        }
+
+        [Test]
+        public void TriangleGetArea_ReturnsCorrectValue()
+        {
+            Triangle triangle = new Triangle(3, 4, 5);
+            double expectedArea = 6;
+            double actualArea = triangle.GetArea();
+            Assert.AreEqual(expectedArea, actualArea, 0.000001);
+        }
+
+        [Test]
+        public void TriangleIsRightTriangle_ReturnsTrue()
+        {
+            Triangle triangle = new Triangle(3, 4, 5);
+            bool isRightTriangle = triangle.IsRightTriangle();
+            Assert.IsTrue(isRightTriangle);
+        }
+
+        [Test]
+        public void TriangleIsRightTriangle_ReturnsFalse()
+        {
+            Triangle triangle = new Triangle(2, 3, 4);
+            bool isRightTriangle = triangle.IsRightTriangle();
+            Assert.IsFalse(isRightTriangle);
+        }
+    }
+}
